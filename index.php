8beee50d855d4907bc7f7843ac46252184ccaf3a
danny-g04 <guodanny04@gmail.com>
2025-11-03 18:33:10 -0800

    dynamically builds entree card from db

--- index.php
+++ index.php
@@ -19,136 +19,101 @@
     </header>
 
     <h2 class="center">Entrees</h2>
+    <?php
+        include("server.php");
+        $sql = "SELECT * from food";
+        $result = mysqli_query($conn, $sql);
+    ?>
 
-
-    <!-- <TABLE class="menu-table">
-<h2 class="center">Entrees</h2>
-<div class="content">
-
-  <div class="card">
-    <img src="https://www.washingtonpost.com/wp-apps/imrs.php?src=https%3A%2F%2Farc-anglerfish-washpost-prod-washpost.s3.amazonaws.com%2Fpublic%2FM6HASPARCZHYNN4XTUYT7H6PTE.jpg?h=982&w=1200" width="100%" alt="Hamburger">
-    <h3>Hamburger</h3>
-    <p><strong>Price:</strong> $10.99</p>
-    <p><strong>Calories:</strong> 900</p>
-  </div>
-
-  <div class="card">
-    <img src="https://newyorkprimebeef.com/cdn/shop/files/new-york-prime-beef-steaks-Dry-Aged-USDA-Prime-Boneless-Ny-Strip-Cooked-Horiz.jpg?v=1756232491" width="100%" alt="New York Strip">
-    <h3>New York Strip</h3>
-    <p><strong>Price:</strong> $19.99</p>
-    <p><strong>Calories:</strong> 1100</p>
-  </div>
-
-  <div class="card">
-    <img src="https://nutritionrefined.com/wp-content/uploads/2023/08/homemade-garden-salad-featured.jpg" width="100%" alt="Salad">
-    <h3>Salad</h3>
-    <p><strong>Price:</strong> $8.99</p>
-    <p><strong>Calories:</strong> 350</p>
-  </div>
-
-  <div class="card">
-    <img src="https://feedgrump.com/wp-content/uploads/2024/01/dairy-queen-chicken-strips-feature.jpg" width="100%" alt="Chicken Strips">
-    <h3>Chicken Strips</h3>
-    <p><strong>Price:</strong> $9.99</p>
-    <p><strong>Calories:</strong> 600</p>
-  </div>
-</div>
-
-     <h2 class="center">Entrees</h2>
-     <TABLE class="menu-table" >
-        <TR>
-            <TH> </TH>
-            <TH>Food</TH>
-            <TH>Price</TH>
-            <TH>Calories</TH>
-        </TR>
-        <TR>
-            <TD><img src="https://www.washingtonpost.com/wp-apps/imrs.php?src=https%3A%2F%2Farc-anglerfish-washpost-prod-washpost.s3.amazonaws.com%2Fpublic%2FM6HASPARCZHYNN4XTUYT7H6PTE.jpg?h=982&w=1200" width="75" height="75"></TD>
-            <TD>Hamburger</TD>
-            <TD>$10.99</TD>
-            <TD>900</TD>
-        </TR>
-        <TR>
-            <TD><img src="https://newyorkprimebeef.com/cdn/shop/files/new-york-prime-beef-steaks-Dry-Aged-USDA-Prime-Boneless-Ny-Strip-Cooked-Horiz.jpg?v=1756232491" width="75" height="75"></TD>
-            <TD>New York Strip</TD>
-            <TD>$19.99</TD>
-            <TD>1100</TD>
-        </TR>
-        <TR>
-            <TD><img src="https://nutritionrefined.com/wp-content/uploads/2023/08/homemade-garden-salad-featured.jpg" width="75" height="75"></TD>
-            <TD>Salad</TD>
-            <TD>$8.99</TD>
-            <TD>350</TD>
-        </TR>
-        <TR>
-            <TD><img src="https://feedgrump.com/wp-content/uploads/2024/01/dairy-queen-chicken-strips-feature.jpg" width="75" height="75"></TD>
-            <TD>Chicken Strips</TD>
-            <TD>$9.99</TD>
-            <TD>600</TD>
-        </TR>
-    </TABLE> -->
+    <div class="content">
+        <?php
+        if (mysqli_num_rows($result) > 0){
+            $img = [
+                'Hamburger' => "https://www.washingtonpost.com/wp-apps/imrs.php?src=https%3A%2F%2Farc-anglerfish-washpost-prod-washpost.s3.amazonaws.com%2Fpublic%2FM6HASPARCZHYNN4XTUYT7H6PTE.jpg?h=982&w=1200",
+                'New York Strip' => "https://newyorkprimebeef.com/cdn/shop/files/new-york-prime-beef-steaks-Dry-Aged-USDA-Prime-Boneless-Ny-Strip-Cooked-Horiz.jpg?v=1756232491", 
+                'Salad' => "https://nutritionrefined.com/wp-content/uploads/2023/08/homemade-garden-salad-featured.jpg",
+                'Chicken Strips' => "https://feedgrump.com/wp-content/uploads/2024/01/dairy-queen-chicken-strips-feature.jpg"
+            ];
+            while($row = mysqli_fetch_assoc($result)){
+                echo '<div class = "card">';
+                echo '<img src ='. $img[$row['food_name']].' width="100%" >';
+                echo '<h3>' . $row['food_name'] .'</h3>';
+                echo '<p>Price:$' . $row['current_price'].'</p>';
+                echo '<p>Calories:' . $row['calories'].'</p>';
+                echo '</div>';
+            }
+        }
+         ?>
+    </div>
 
     <div class="bottom-tables">
-    <h2 class="center">Drinks</h2>
-    <TABLE class="drink-table center-table">
-        <TR>
-            <TH> </TH>
-            <TH>Drink</TH>
-            <TH>Price</TH>
-            <TH>Calories</TH>
-        </TR>
-        <TR>
-            <TD><img src="https://t4.ftcdn.net/jpg/07/01/77/07/360_F_701770789_iUaOh1Bf5BqxVhP1nBXPxjWTBo19y3cO.jpg" width="75" height="75"></TD>
-            <TD>Lemonade</TD>
-            <TD>$2.99</TD>
-            <TD>90</TD>
-        </TR>
-        <TR>
-            <TD><img src="https://media.istockphoto.com/id/530428650/photo/cola-glass-with-ice-cubes.jpg?s=612x612&w=0&k=20&c=keqH2KNWHO1sFxtsBfx5EZjyep1CRBHIqwe_ZwxszHc=" width="75" height="90"></TD>
-            <TD>Soda</TD>
-            <TD>$1.99</TD>
-            <TD>100</TD>
-        </TR>
-        <TR>
-            <TD><img src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/pint-glass-of-beer-burazin.jpg" width="75" height="85"></TD>
-            <TD>Beer</TD>
-            <TD>$5.99</TD>
-            <TD>150</TD>
-        </TR>
-        <TR>
-            <TD><img src="https://www.ohhowcivilized.com/wp-content/uploads/iced-black-tea-recipe.jpg" width="75" height="75"></TD>
-            <TD>Tea</TD>
-            <TD>$1.99</TD>
-            <TD>20</TD>
-        </TR>
-    </TABLE>
-    <h2 class="center">Desserts</h2>
-    <TABLE class="drink-table center-table">
-        <TR>
-            <TH> </TH>
-            <TH>Desserts</TH>
-            <TH>Price</TH>
-            <TH>Calories</TH>
-        </TR>
-        <TR>
-            <TD><img src="https://redribbonbakeshop.com/cdn/shop/files/RR-Strawberry-Dream-Shopify-slice.png?v=1714605743&width=1080" width="75" height="75"></TD>
-            <TD>Cake</TD>
-            <TD>$4.99</TD>
-            <TD>290</TD>
-        </TR>
-        <TR>
-            <TD><img src="https://www.thespruceeats.com/thmb/3QatTcsVjKeDVCD-rIeUa4fSRAs=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/perfect-banana-split-recipe-305712-hero-01-ef0482a539394da0b5ba64ade0c73b98.jpg" width="75" height="75"></TD>
-            <TD>Banana Split</TD>
-            <TD>$3.99</TD>
-            <TD>180</TD>
-        </TR>
-        <TR>
-            <TD><img src="https://media.istockphoto.com/id/184350005/photo/slice-of-apple-pie-on-a-plate-isolalted-on-a-white-background.jpg?s=612x612&w=0&k=20&c=X140KNLW8dnMP8XztKyk3dNSioqY6aZ2VGn3BhRu68s=" width="75" height="75"></TD>
-            <TD>Pie</TD>
-            <TD>$5.99</TD>
-            <TD>350</TD>
-        </TR>
-    </TABLE>
+        <h2 class="center">Drinks</h2>
+        <TABLE class="drink-table center-table">
+            <TR>
+                <TH> </TH>
+                <TH>Drink</TH>
+                <TH>Price</TH>
+                <TH>Calories</TH>
+            </TR>
+            <TR>
+                <TD><img src="https://t4.ftcdn.net/jpg/07/01/77/07/360_F_701770789_iUaOh1Bf5BqxVhP1nBXPxjWTBo19y3cO.jpg"
+                        width="75" height="75"></TD>
+                <TD>Lemonade</TD>
+                <TD>$2.99</TD>
+                <TD>90</TD>
+            </TR>
+            <TR>
+                <TD><img src="https://media.istockphoto.com/id/530428650/photo/cola-glass-with-ice-cubes.jpg?s=612x612&w=0&k=20&c=keqH2KNWHO1sFxtsBfx5EZjyep1CRBHIqwe_ZwxszHc="
+                        width="75" height="90"></TD>
+                <TD>Soda</TD>
+                <TD>$1.99</TD>
+                <TD>100</TD>
+            </TR>
+            <TR>
+                <TD><img src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/2/pint-glass-of-beer-burazin.jpg"
+                        width="75" height="85"></TD>
+                <TD>Beer</TD>
+                <TD>$5.99</TD>
+                <TD>150</TD>
+            </TR>
+            <TR>
+                <TD><img src="https://www.ohhowcivilized.com/wp-content/uploads/iced-black-tea-recipe.jpg" width="75"
+                        height="75"></TD>
+                <TD>Tea</TD>
+                <TD>$1.99</TD>
+                <TD>20</TD>
+            </TR>
+        </TABLE>
+        <h2 class="center">Desserts</h2>
+        <TABLE class="drink-table center-table">
+            <TR>
+                <TH> </TH>
+                <TH>Desserts</TH>
+                <TH>Price</TH>
+                <TH>Calories</TH>
+            </TR>
+            <TR>
+                <TD><img src="https://redribbonbakeshop.com/cdn/shop/files/RR-Strawberry-Dream-Shopify-slice.png?v=1714605743&width=1080"
+                        width="75" height="75"></TD>
+                <TD>Cake</TD>
+                <TD>$4.99</TD>
+                <TD>290</TD>
+            </TR>
+            <TR>
+                <TD><img src="https://www.thespruceeats.com/thmb/3QatTcsVjKeDVCD-rIeUa4fSRAs=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/perfect-banana-split-recipe-305712-hero-01-ef0482a539394da0b5ba64ade0c73b98.jpg"
+                        width="75" height="75"></TD>
+                <TD>Banana Split</TD>
+                <TD>$3.99</TD>
+                <TD>180</TD>
+            </TR>
+            <TR>
+                <TD><img src="https://media.istockphoto.com/id/184350005/photo/slice-of-apple-pie-on-a-plate-isolalted-on-a-white-background.jpg?s=612x612&w=0&k=20&c=X140KNLW8dnMP8XztKyk3dNSioqY6aZ2VGn3BhRu68s="
+                        width="75" height="75"></TD>
+                <TD>Pie</TD>
+                <TD>$5.99</TD>
+                <TD>350</TD>
+            </TR>
+        </TABLE>
     </div>
 </body>
 
