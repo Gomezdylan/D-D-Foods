b910b153c8ed6f1c76bf0eba3cc64353848e538d
danny-g04 <guodanny04@gmail.com>
2025-11-03 18:32:43 -0800

    database

--- schema.sql
+++ schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE
 
 CREATE TABLE
     Food (
-        food_id INT AUTO_INCREMENT PRIMARY KEY,
+        food_id INT AUTO_INCREMENT PRIMARFY KEY,
         food_name VARCHAR(100) NOT NULL,
         category VARCHAR(50),
         current_price DECIMAL(6, 2) NOT NULL,
