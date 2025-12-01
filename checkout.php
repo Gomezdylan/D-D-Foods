ceaf4b9ce60bfaf96b0c0081fe2778ed6d699f11
Gomezdylan <dmgomezschool@gmail.com>
2025-12-01 13:39:14 -0800

    Final correct version after fixing rebase

--- checkout.php
+++ checkout.php
@@ -58,22 +58,34 @@
                 }
                 </style>
             <br>
+            <label>
+                <input type="checkbox" id="dessert">
+                Dessert Included?
+              </label>
+            <style>
+                input[type="checkbox"] {
+                  width: 18px;
+                  height: 18px;
+                }
+                </style>
+            <br>
         
             <label for="fname">First name:</label>
             <input type="text" style="height: 40px;" id="fname" name="fname"> <br></br>
-            <label for="fname">Last name:</label>
-            <input type="text"style="height: 40px;" id="fname" name="fname"> <br></br>
+            <label for="lname">Last name:</label>
+            <input type="text"style="height: 40px;" id="lname" name="lname"> <br></br>
         
-            <label for="fname">Phone Number:</label>
-            <input type="text"style="height: 40px;" id="fname" name="fname"> <br></br>
-            <label for="fname">Email</label>
-            <input type="text" style="height: 40px;"id="fname" name="fname">
+            <label for="pnumber">Phone Number:</label>
+            <input type="text"style="height: 40px;" id="pNumber" name="pNumber"> <br></br>
+            <label for="em">Email</label>
+            <input type="text" style="height: 40px;"id="em" name="em">
 
             <!--Firs name last name shipping address zip city state phone email-->
 
             <!--This should all be on the left side of the page-->
         </div>
         <div>
+<<<<<<< HEAD
             <h1>Payment</h1>
             <div>
                 <img src="img/burger3.jpg" height="300" width="300"/>
@@ -84,7 +96,28 @@
             <p>Thanks for shopping with D&D Foods!</p>
         </div>
     </section>
+=======
+  <h1>Payment</h1>
+  <div>
+    <img src="burger3.jpg" height="300" width="300"/>
+    <h1 id="total">Total Payment: $0.00 </h1>
+  </div>
+>>>>>>> 29fd655 (changes)
 
+  <form id="checkoutForm" action="add_order_item.php" method="post">
+    <input type="text" style="height: 40px;" placeholder="Enter discount or code">
+    
+    <input type="hidden" name="price_each" id="price_each">
+    <input type="hidden" name="order_id" id="order_id" value="1">
+    
+    <button id="buyBtn" type="submit">Buy Now</button>
+  </form>
+
+  <p id="thanks"></p>
+</div>
+
+    </section>
+<!--This script is just for dynamically changing the price-->
     <script>
         // we need the selected food
         const foodSelected = document.querySelector("select[name='food_id']");
@@ -95,6 +128,7 @@
         // is there an side
         const sideBox = document.getElementById("side");
         // we have to adjust the total amount at the third header I believe
+        const dessertBox = document.getElementById("dessert");
         const totalPrice = document.getElementById("total");
 
 
@@ -115,16 +149,36 @@
             if(sideBox.checked){
                 total += 3.00;
             }
-
+            if(dessertBox.checked){
+                total += 5.00;
+            }
             totalPrice.textContent = "Total Payment: $" + total.toFixed(2);
         }
-
         foodSelected.addEventListener("change", updatePrice);
         qtyInput.addEventListener("input", updatePrice);
         drinkBox.addEventListener("change", updatePrice);
         sideBox.addEventListener("change", updatePrice);
+        dessertBox.addEventListener("change", updatePrice);
 
 
+    </script>
+    <script>
+        // what id you are getting
+        const buyBtn = document.getElementById("buyBtn");
+        const nameInput = document.getElementById("fname");
+        const thanks = document.getElementById("thanks");
+
+        buyBtn.addEventListener("click", () => {
+            const name = nameInput.value.trim();
+            if(name){
+                thanks.textContent = `Thank you for shopping with D&D Foods, ${name}!`;
+            } 
+        });
+
+        // func
+
+        // event Listener.... they always follow this in JS scripting
+    
     </script>
 </body>
 </html>
